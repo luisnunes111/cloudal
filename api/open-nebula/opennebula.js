ef3630d7e09052a4408f1c15e3283bf339c0d6f2
Luis Nunes <luisnunes111@hotmail.com>
2018-06-18 01:05:48 +0100

    VM Dashboard page

--- api/open-nebula/opennebula.js
+++ api/open-nebula/opennebula.js
@@ -34,7 +34,32 @@ exports.getInfoVM = function getInfoVM(id) {
       if (err) {
         reject(err);
       } else {
-        resolve(data);
+        let status = "pending";
+        if (parseInt(data.VM.STATE) >= 3 && parseInt(data.VM.LCM_STATE) === 3) {
+          state = "running";
+        } else if (
+          parseInt(data.VM.STATE) === 8 &&
+          parseInt(data.VM.LCM_STATE) === 0
+        ) {
+          state = "stopped";
+        } else if (
+          parseInt(data.VM.STATE) === 5 &&
+          parseInt(data.VM.LCM_STATE) === 0
+        ) {
+          state = "paused";
+        }
+
+        let res = {
+          id: data.VM.ID,
+          name: data.VM.NAME,
+          status: status,
+          template: {
+            id: data.VM.TEMPLATE.TEMPLATE_ID,
+            cpu: data.VM.TEMPLATE.CPU,
+            memory: data.VM.TEMPLATE.MEMORY
+          }
+        };
+        return resolve(res);
       }
     });
   }).catch(err => new Error(err));
@@ -87,6 +112,36 @@ exports.deleteVM = function deleteVM(id) {
         reject(err);
       } else {
         resolve(data);
+      }
+    });
+  }).catch(err => new Error(err));
+};
+
+exports.monitoringVM = function monitoringVM(id) {
+  return new Promise((resolve, reject) => {
+    const vm = one.getVM(parseInt(id));
+
+    vm.monitoring(function(err, data) {
+      if (err) {
+        //'Error fetching VM statistics'
+        reject(err);
+      } else {
+        let stats = [];
+
+        if (data.MONITORING_DATA && data.MONITORING_DATA.VM) {
+          const totalStats = data.MONITORING_DATA.VM.length;
+          for (var i = 0; i < totalStats; i++) {
+            stats.push({
+              time: data.MONITORING_DATA.VM[i].LAST_POLL,
+              cpu: data.MONITORING_DATA.VM[i].MONITORING.CPU,
+              memory: data.MONITORING_DATA.VM[i].MONITORING.MEMORY,
+              nettx: data.MONITORING_DATA.VM[i].MONITORING.NETTX,
+              netrx: data.MONITORING_DATA.VM[i].MONITORING.NETRX
+            });
+          }
+        }
+        stats = stats.slice(Math.max(stats.length - 40, 0)); //last 40
+        return resolve(stats);
       }
     });
   }).catch(err => new Error(err));
